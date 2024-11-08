2c4efa18f9bc30fd7a6febce6754613506fa4bb8
gautam-404 <gautam.anuj.1195@gmail.com>
2024-11-09 05:08:36 +0530

    first build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://gautam-404.github.io',
-    base: 'https://github.com/gautam-404/gautam-404.github.io',
+    // base: 'https://github.com/gautam-404/gautam-404.github.io',
   })
